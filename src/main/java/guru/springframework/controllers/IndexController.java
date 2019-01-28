c0b06ba0e17ff4872b7afa532dcbd33684f5969c
Cor3Man <lisockij@gmail.com>
2019-01-28 22:09:27 +0300

    tested IndexController (Mockito)

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -17,26 +17,16 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private final CategoryRepository categoryRepository;
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
     private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository,
-            RecipeService recipeService)
+    public IndexController(RecipeService recipeService)
     {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
         this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
         log.debug("I'm the Controller");
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Pinch");
-        log.debug("Cat Id is:" + categoryOptional.get().getId());
-        log.debug("UOM Id is:" + unitOfMeasureOptional.get().getId());
 
         model.addAttribute("recipies", recipeService.getAllRecipies());
 
